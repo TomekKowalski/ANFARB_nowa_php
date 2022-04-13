77957473ffaaceb5018316e29d7bffc7abde918e
TomekKowalski <t.kowalski7411@gmail.com>
2022-04-13 21:06:57 +0200

    edycja 2 script 13.04.2022

--- js/script_pokaz_slajdow.js
+++ js/script_pokaz_slajdow.js
@@ -9,9 +9,9 @@ window.onload = function()
     //imageRight.innerHTML = "test";
     //slideShowCenter.innerHTML += "<br>";
     
-    var imagesSrc = ["image_left_01.jpg", "bg-farbiarnia3.jpg"];
-    var imagesSrcCenter = ["image_center_01 (1).jpg", "image_center_01 (2).jpg"];
-    var imagesSrcRight = ["image_right01.jpg", "image_right02.jpg"];
+    var imagesSrc = ["image_left_01.jpg", "bg-farbiarnia3.jpg", "image_left_04.jpg", "image_left_06.jpg"];
+    var imagesSrcCenter = ["image_center_01 (1).jpg", "image_center_01 (2).jpg", "image_center_01 (3).jpg", "image_center_01 (4).jpg"];
+    var imagesSrcRight = ["image_right01.jpg", "image_right02.jpg", "image_right03.jpg", "image_right04.jpg"];
     
     
     for(var i=0; i<imagesSrc.length; i++)
@@ -39,32 +39,52 @@ window.onload = function()
     imageRight.childNodes[0].setAttribute("class", "currentRight");
     
     var i = 0;
+    var i_left = 0;
+    var i_center = 0;
     var flaga_left = true;
     var flaga_center = false;
     var flaga_right = false;
     var flaga_zmien = false;
+    
+    var temp = 0;
+    var temp_plus = 0;
     setInterval(function()
     {
      
         if(flaga_left)
         {
-            slideShow.childNodes[i % imagesSrc.length].setAttribute("class", "");   
-            slideShow.childNodes[(i+1) % imagesSrc.length].setAttribute("class", "current");
+            slideShow.childNodes[i_left%imagesSrc.length].setAttribute("class", "");   
+            slideShow.childNodes[(i_left+1)%imagesSrc.length].setAttribute("class", "current");
+            //temp = i_left%imagesSrc.length;
+            //temp_plus = (i_left+1)%imagesSrc.length;
+            //slideShow.innerHTML = "test";
+            //slideShow.innerHTML += i;
+            /*
+            slideShow.innerHTML += temp;
+            slideShow.innerHTML += "<br>";
+            slideShow.innerHTML += "test_plus";
+            slideShow.innerHTML += temp_plus;
+             * 
+             */
+            
             flaga_zmien = true;
+            i_left++;
             
         }
     
         if(flaga_center)
         {
-            imageCenter.childNodes[i % imagesSrcCenter.length].setAttribute("class", "");   
-            imageCenter.childNodes[(i+1) % imagesSrcCenter.length].setAttribute("class", "currentCenter");
-            flaga_zmien = true;          
+            imageCenter.childNodes[i_center%imagesSrcCenter.length].setAttribute("class", "");   
+            imageCenter.childNodes[(i_center+1)%imagesSrcCenter.length].setAttribute("class", "currentCenter");
+            flaga_zmien = true; 
+            i_center++;
         }
         if(flaga_right)
         {
             imageRight.childNodes[i % imagesSrcCenter.length].setAttribute("class", "");   
             imageRight.childNodes[(i+1) % imagesSrcCenter.length].setAttribute("class", "currentRight");
             flaga_zmien = true;
+            i++;
         }
         if(flaga_left && flaga_zmien)
         {
@@ -85,7 +105,8 @@ window.onload = function()
             flaga_zmien = false;
         }
     
-        i++;
+        
+        
         
     }, 10000);
         
